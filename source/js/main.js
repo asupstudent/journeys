574db6c1171c1c83479fd8bb43ecb8991816b72c
asupstudent <asupstudent@yandex.ru>
2023-12-04 16:18:05 +0300

    Подключает слайдер, пока без детальной настройки

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,11 @@
 import {iosVhFix} from './utils/ios-vh-fix';
+import {initSliderHero} from './sliders/init-slider-hero';
 import {Form} from './modules/form-validate/form';
 
 window.addEventListener('DOMContentLoaded', () => {
   iosVhFix();
   window.addEventListener('load', () => {
+    initSliderHero();
     const form = new Form();
     window.form = form;
     form.init();
---
+++ source/js/sliders/init-slider-hero.js
@@ -0,0 +1,22 @@
+const sliderHero = document.querySelector('[data-hero-slider="hero-slider"]');
+const sliderHeroPagination = document.querySelector('[data-hero-pagination="hero-pagination"]');
+
+const setSliderHero = () => {
+  return new window.Swiper(sliderHero, {
+    pagination: {
+      el: sliderHeroPagination,
+      clickable: true,
+    },
+    centeredSlides: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+  });
+};
+
+const initSliderHero = () => {
+  if (document.body.contains(sliderHero)) {
+    setSliderHero();
+  }
+};
+
+export {initSliderHero};
