b4a6ec0e77035b2e56a99846d58c1ae76bee9615
asupstudent <asupstudent@yandex.ru>
2023-12-07 14:18:25 +0300

    Почти заканчивает шапку сайта, осталось focus-lock и no-js

---
+++ source/js/burger/init-menu.js
@@ -0,0 +1,44 @@
+import '../utils/scroll-lock';
+import '../utils/focus-lock';
+
+const toggleBurgerMenu = () => {
+  const body = document.body;
+  const mainNav = document.querySelector('[data-menu="main-nav"]');
+  const logo = document.querySelector('[data-menu="logo"]');
+  const navLists = document.querySelectorAll('[data-menu="nav-list"]');
+  const phone = document.querySelector('[data-menu="phone"]');
+  const buttons = document.querySelectorAll('[data-menu="burger-button"]');
+
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      mainNav.classList.toggle('nav--is-closed');
+      mainNav.classList.toggle('nav--is-opened');
+      logo.classList.toggle('logo--is-closed');
+      logo.classList.toggle('logo--is-opened');
+      phone.classList.toggle('nav__phone--is-closed');
+      phone.classList.toggle('nav__phone--is-opened');
+      body.classList.toggle('overlay');
+      navLists.forEach((navList) => {
+        const menuIsClosed = navList.classList.contains('nav__list--is-closed');
+        const closedIconMenu = document.querySelector('[data-menu="is-closed"]');
+        const openedIconMenu = document.querySelector('[data-menu="is-opened"]');
+
+        if (menuIsClosed) {
+          navList.classList.remove('nav__list--is-closed');
+          navList.style.display = 'flex';
+          closedIconMenu.style.display = 'none';
+          openedIconMenu.style.display = 'block';
+          window.scrollLock.disableScrolling();
+        } else {
+          navList.classList.add('nav__list--is-closed');
+          navList.style.display = 'none';
+          closedIconMenu.style.display = 'block';
+          openedIconMenu.style.display = 'none';
+          window.scrollLock.enableScrolling();
+        }
+      });
+    });
+  });
+};
+
+export {toggleBurgerMenu};
--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,12 @@
 import {iosVhFix} from './utils/ios-vh-fix';
+import {toggleBurgerMenu} from './burger/init-menu';
 import {initSliderHero} from './sliders/init-slider-hero';
 import {Form} from './modules/form-validate/form';
 
 window.addEventListener('DOMContentLoaded', () => {
   iosVhFix();
   window.addEventListener('load', () => {
+    toggleBurgerMenu();
     initSliderHero();
     const form = new Form();
     window.form = form;
