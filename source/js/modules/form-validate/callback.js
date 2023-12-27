6899f3b73da24b826b2810b9be18e0ea0ca68e01
asupstudent <asupstudent@yandex.ru>
2023-12-27 23:01:52 +0300

    Начинается реализация валидации формы

--- source/js/modules/form-validate/callback.js
+++ source/js/modules/form-validate/callback.js
@@ -1,16 +1,18 @@
 const baseSuccessCallback = (event) => {
   event.preventDefault();
+  const form = document.querySelector('[data-form-feedback]');
   const url = 'https://echo.htmlacademy.ru/';
-  const formData = new FormData(document.querySelector('[data-form-feedback]'));
-
+  const formData = new FormData(form);
   fetch(url, {
     method: 'POST',
     body: formData,
   }).then((response) => response.text()).then((data) => {
     document.body.innerHTML = `<pre>${data}</pre>`;
   }).catch(() => {
-    document.body.innerHTML = 'Ошибка: не удалось отправить форму на сервер';
+
   });
+  form.noValidate = false;
+  form.reportValidity();
 };
 
 const baseErrorCallback = (event) => {
@@ -19,7 +21,7 @@ const baseErrorCallback = (event) => {
 
 export const callbacks = {
   base: {
-    reset: true,
+    reset: false,
     resetTimeout: 500,
     successCallback: baseSuccessCallback,
     errorCallback: baseErrorCallback,
